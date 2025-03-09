eb8f9048c1bc30cebe6e44a446314f56b2fda747
Domingos Santiago <dmgssantiago@gmail.com>
2025-03-09 16:49:02 -0300

    criação dos componentes

---
+++ src/app/components/main-card/main-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-card',
+  imports: [],
+  templateUrl: './main-card.component.html',
+  styleUrl: './main-card.component.css'
+})
+export class MainCardComponent {
+
+}
