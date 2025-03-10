0f0506a8c780d5d56c0d3c288ba59030c7c1c8f1
Domingos Santiago <dmgssantiago@gmail.com>
2025-03-09 22:14:21 -0300

    Estilização dos componentes, criação da home

---
+++ src/app/components/browse-topics/browse-topics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PostCardComponent } from "../post-card/post-card.component";
+
+@Component({
+  selector: 'app-browse-topics',
+  imports: [PostCardComponent],
+  templateUrl: './browse-topics.component.html',
+  styleUrl: './browse-topics.component.css'
+})
+export class BrowseTopicsComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { MainCardComponent } from "../../components/main-card/main-card.component"
+import { SmallCardComponent } from "../../components/small-card/small-card.component"
+import { BrowseTopicsComponent } from "../../components/browse-topics/browse-topics.component"
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+import { FooterComponent } from "../../components/footer/footer.component"
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitleComponent, MainCardComponent],
+  imports: [MenuTitleComponent, MainCardComponent, SmallCardComponent, BrowseTopicsComponent, MenuBarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
